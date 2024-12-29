f724c050b7e5bbbf3511bed20bdc80bcc789f415
Joerow <Joerow@gmail.com>
2024-12-29 09:38:28 +0800

    add greeting

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { Greeting } from "./greeting.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Greeting />
   </StrictMode>
 );
