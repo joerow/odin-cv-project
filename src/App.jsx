818195b34692cab891b743851e85cd5e3e7b4aa4
Joerow <Joerow@gmail.com>
2024-12-30 20:58:43 +0800

    style: enhance layout and styling for App component and forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
-import { useState } from "react";
 import "./App.css";
 import PersonalInfo from "./PersonalInfo";
 import EducationalExperience from "./EducationalExperience";
 import PracticalExperience from "./PracticalExperience";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
+    <div className="container">
       <h1>CV App</h1>
       <PersonalInfo />
       <EducationalExperience />
       <PracticalExperience />
-    </>
+    </div>
   );
 }
 
